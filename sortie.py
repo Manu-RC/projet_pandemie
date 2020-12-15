5b0c950955de6bfef1c8429249b5d879931624d3
Taha-El <75489426+Taha-El@users.noreply.github.com>
2020-12-15 17:56:28 +0100

    Add files via upload

--- sortie.py
+++ sortie.py
@@ -3,21 +3,28 @@ from universe import Universe
 from simulation import Simulation
 from Maladie import Maladie
 
+from individu import Individu
+
+
 class Sortie : 
 
-    def __init__(self):
+    def __init__(self,dimension_x,dimension_y,nombre_individus,rayon,refresh_time):
 
         self.ui = Ui_Form()
         
+        simulation = Simulation(dimension_x,dimension_y)
+        simulation.generation(rayon,nombre_individus)
         
-        maladie = Maladie(0)
-        simulation = Simulation(maladie,500,500)
-        univers = Universe(simulation)
-        self.ui.setupUi(univers)
-        self.ui.graphicsView.setScene(univers.scene)
+        self.univers = Universe(simulation,refresh_time)
+        self.ui.setupUi(self.univers)
+        self.ui.graphicsView.setScene(self.univers.scene)
+
+        # univers = Universe(simulation)
+        # self.ui.setupUi(univers)
+        # self.ui.graphicsView.setScene(univers.scene)
 
     
-        self.ui.StartButton.clicked.connect(univers.stop)
-        self.ui.Stopbutton.clicked.connect(univers.start)
+        self.ui.StartButton.clicked.connect(self.univers.start)
+        self.ui.Stopbutton.clicked.connect(self.univers.stop)
 
 
--- universe.py
+++ universe.py
@@ -7,47 +7,49 @@ from PyQt5.QtGui import QPen, QBrush, QColor
 
 class Universe(QtWidgets.QWidget) :
 
-    def __init__(self,simu):
+    def __init__(self,simu,refresh_time):
 
         super().__init__()
         self.simu = simu
         self.length = simu.x_max
         self.height = simu.y_max
         self.setWindowTitle("test")
-        # self.resize(l,h)
         self.scene = QtWidgets.QGraphicsScene()
-        # self.items = QtWidgets.QGraphicsItemGroup()
+        self.items = QtWidgets.QGraphicsItemGroup()
         self.scene.addItem(self.items)
         self.timer = QtCore.QTimer(self)
-        self.timer.timeout.connect(self.simu.advance())
-        # self.add_rd_people()
+        self.refresh_time = refresh_time
+        self.timer.timeout.connect(self.update_people)
+        self.set_people()
+
+        # self.timer.timeout.connect(self.simu.advance)
+
+    
+
         self.add_shortcut('f', lambda: self.playpause())
         
+    def set_people(self):
 
-    # def add_rd_people(self):
+        group = QtWidgets.QGraphicsItemGroup()
+        self.scene.addItem(group)
 
-    #     self.scene.clear()
-    #     group = QtWidgets.QGraphicsItemGroup()
-    #     self.scene.addItem(group)
+        for individu in self.simu.population:
 
-    #     for i in range(10):
-
-    #         rd_x = rd.uniform(10,self.length)
-    #         rd_y = rd.uniform(10,self.height) 
-    #         bounds = QtCore.QRectF(rd_x,rd_y, 10, 10)
-    #         item = QtWidgets.QGraphicsEllipseItem(bounds, group )
-    #         item.setBrush(QBrush(QColor("red")))
-
+            bounds = QtCore.QRectF(individu.x,individu.y,individu.rayon*2,individu.rayon*2)
+            item = QtWidgets.QGraphicsEllipseItem(bounds, group)
+            item.setBrush(QBrush(QColor("red")))
+    
+    
     def update_people(self):  
         """met à jour les emplacements des individus dans la scene"""
-
+        self.simu.advance()
         self.scene.clear()
         group = QtWidgets.QGraphicsItemGroup()
         self.scene.addItem(group)
 
         for individu in self.simu.population:
 
-            bounds = QtCore.QRectF(individu.x,individu.y,individu.rayon,individu.rayon)
+            bounds = QtCore.QRectF(individu.x,individu.y,individu.rayon*2,individu.rayon*2)
             item = QtWidgets.QGraphicsEllipseItem(bounds, group)
             item.setBrush(QBrush(QColor("red")))  #pourra etre modifié par la suite
 
@@ -62,17 +64,17 @@ class Universe(QtWidgets.QWidget) :
         if self.timer.isActive():
             self.timer.stop()
         else:
-            self.timer.start(500)
+            self.timer.start(self.refresh_time)
 
 
     def start(self):
 
         if not self.timer.isActive():
 
-            self.timer.start(500)
+            self.timer.start(self.refresh_time)
 
     def stop(self):
 
         if self.timer.isActive():
 
-            self.timer.stop
+            self.timer.stop()
