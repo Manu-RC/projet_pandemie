be27813a1c894c9d325e81735626643cabf85a51
erwan-coding <75489458+erwan-coding@users.noreply.github.com>
2021-01-06 15:38:17 +0100

    Add files via upload

--- politique.py
+++ politique.py
@@ -1,11 +1,24 @@
 from individu import Individu
 
-def confinement(simulation):
+def isolement(simulation):
 
     for individu in simulation.population:
         if individu.etat != "Infecte":
             individu.move(simulation.x_max,simulation.y_max)
+    simulation.time += simulation.time_increment
 
+def couvre_feu(simulation):
+
+    if (simulation.time % 24) < 20:
+        for individu in simulation.population:
+            individu.move(simulation.x_max,simulation.y_max)
+    simulation.time += simulation.time_increment
+
+def pas_de_politique(simulation):
+
+    for individu in simulation.population :
+        individu.move(simulation.x_max,simulation.y_max)
+    simulation.time += simulation.time_increment
 
 
 
