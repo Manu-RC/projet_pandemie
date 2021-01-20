7cd9398105f379f002534b74e6a75b6331b3c5c0
Manu-RC <75321088+Manu-RC@users.noreply.github.com>
2021-01-21 00:00:30 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from Maladie import Maladie
+from maladie import Maladie
 from sortie import Sortie
 import sys
 from PyQt5 import QtWidgets
@@ -6,21 +6,11 @@ from PyQt5 import QtWidgets
 
 def main(longueur,largeur,nombre_individus,nombre_contamines_init,borne_vitesse_init,taux_respect_rules,hit_time,taux_contagion,mutation_init,duree_transmissibilite_init,letalite):
 
-    #maladie initiale
-    #hit_time = 0
-    #Taux_contagion = 0.5
-    #muta_init = 0.01
-    #Duree_transmissibilite = 200
-    #lethalite = 1
     maladie_init = Maladie(hit_time,taux_contagion,mutation_init,duree_transmissibilite_init,letalite)
-
     rayon = 4
     refresh_time = 20 #millisecondes
-
     app = QtWidgets.QApplication(sys.argv)
-    
     out = Sortie(longueur,largeur,nombre_individus,rayon,refresh_time,maladie_init,nombre_contamines_init,borne_vitesse_init,taux_respect_rules)
-
     out.univers.show()
     sys.exit(app.exec_())
 
