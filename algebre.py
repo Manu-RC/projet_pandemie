578f800a0aff0332f555210e3b705fb49fd8b833
Manu-RC <75321088+Manu-RC@users.noreply.github.com>
2021-01-21 19:16:23 +0100

    Add files via upload

--- algebre.py
+++ algebre.py
@@ -7,15 +7,22 @@ def uniform(a,b):
     return a + (b-a) * np.random.uniform(0,1)
 
 def gaussienne(facteur):
-	proba = 0.1 * np.random.randn() + facteur #on recentre la loi normale
-	if proba > 1 : return facteur
-	return proba
+    """Sort un nombre compris entre 0 et 1 suivant loi normal spécifique
+        Potentiellement, on peut avoir une valeur en dehors de [0,1]
+        Dans ce cas on affecte la valeur du taux de contagion ou le taux de letalite cf appelle dans simu"""
+    float_rand = 0.1 * np.random.randn() + facteur #on recentre la loi normale moyenne=facteur et 0.1 la variance
+    if float_rand > 1 :
+        return facteur
+    elif float_rand < 0 :
+        return facteur
+    else:
+        return float_rand
 
 def seed():
     np.random.seed()
     
 def binomiale(p):
-     return np.random.binomial(1, p)
+    return np.random.binomial(1, p)
 
 def distance(cercle_1,cercle_2):
     """ Entrée : un couple (position x, position y, rayon)
